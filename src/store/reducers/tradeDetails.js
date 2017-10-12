c5ec58be1b85f38f6a06207cecbde643c72bc9ef
Arkadiusz Szaleniec <arkadiusz.szaleniec@pragmaticcoders.com>
2017-10-12 15:47:29 +0200

    Token exchange rate store updates. Components cleanup

--- src/store/reducers/tradeDetails.js
+++ src/store/reducers/tradeDetails.js
@@ -1,44 +1,56 @@
 import {createAction, handleActions} from 'redux-actions';
 import Immutable from 'immutable';
 
-const SET_TOKEN_PRICE =   'TRADE_DETAILS/SET_TOKEN_PRICE';
 const SET_TRANSACTION_FEE = 'TRADE_DETAILS/SET_TRANSACTION_FEE';
 const SET_TOKEN_UNIT_SYMBOL = 'TRADE_DETAILS/SET_TOKEN_UNIT_SYMBOL';
 const SET_TRANSACTION_MARKET = 'TRADE_DETAILS/SET_TRANSACTION_MARKET';
 
+const SET_TOKEN_EXCHANGE_RATE = 'TRADE_DETAILS/SET_TOKEN_EXCHANGE_RATE';
+const FETCH_BUY_TRANSACTION_DATA = 'TRADE_DETAILS/FETCH_BUY_TRANSACTION_DATA';
+const FETCH_SELL_TRANSACTION_DATA = 'TRADE_DETAILS/FETCH_SELL_TRANSACTION_DATA';
 
-const SetTokenPrice = createAction(
-    SET_TOKEN_PRICE, (tokenPrice) => tokenPrice
-);
 
 const SetTransactionFee = createAction(
-    SET_TRANSACTION_FEE, (transactionFee) => transactionFee
+    SET_TRANSACTION_FEE, (transactionFee) => (d) => transactionFee
 );
 
 const SetTokenPriceUnitSymbol = createAction(
     SET_TOKEN_UNIT_SYMBOL, (unitSymbol) => unitSymbol
-)
-;
+);
 
 const SetTransactionMarket = createAction(
     SET_TRANSACTION_MARKET, (transactionMarket) => transactionMarket
 );
 
+
+const FetchBuyTransactionData = createAction(
+    FETCH_BUY_TRANSACTION_DATA, async (buyToken, receiveToken, amount) => null
+);
+
+/**
+ * Fetch exchange rate for sell transaction
+ */
+const FetchSellTransactionData = createAction(
+    FETCH_SELL_TRANSACTION_DATA, async (sellToken, receiveToken, amount) => null
+);
+
+const SetTokenExchangeRate = createAction(
+    SET_TOKEN_EXCHANGE_RATE,
+    () => null
+)
+
+
 const actions = {
-  SetTokenPrice,
   SetTransactionFee,
   SetTokenPriceUnitSymbol,
-  SetTransactionMarket
+  SetTransactionMarket,
+  FetchBuyTransactionData,
+  FetchSellTransactionData
 };
 
 
 
 const reducer = handleActions({
-  [SetTokenPrice]: (state, {payload}) =>
-      state
-      .update(
-          'tokenPrice', v => payload
-      ),
   [SetTransactionFee]: (state, {payload}) =>
       state
       .update(
@@ -52,14 +64,16 @@ const reducer = handleActions({
   [SetTransactionMarket]: (state, {payload}) =>
       state
       .update('market', v => payload),
-
+  [FetchBuyTransactionData]: (state, {payload}) => state,
+  [FetchSellTransactionData]: (state, {payload}) => state,
+  [SetTokenExchangeRate]: (state) => state
 
 }, Immutable.fromJS(
   {
-    tokenPrice: 1.22332,
-    transactionFee: 0.22,
-    tokenPriceUnitSymbol: 'ETH',
-    market: ' Oasisdex'
+    transactionFee: null,
+    tokenPriceUnitSymbol: null,
+    tokenExchangeRate: 1.2,
+    market: null
   }
 ));
 
